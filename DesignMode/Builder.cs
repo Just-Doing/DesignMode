95d36f4a9e1f5078e47a7ff51ab1787a626b7225
Just-Doing <969502899@qq.com>
2020-03-08 20:56:20 +0800

    建造者模式

---
+++ DesignMode/Builder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignMode_Builder
+{
+    public class HongShaoRou {
+        public int Wendu { get; set; }
+        public int Shui { get; set; }
+        public int You { get; set; }
+    }
+    public abstract class HongshaorouBuilder // 红烧肉
+    {
+        public HongShaoRou hongshaorou { get; set; }
+        abstract public void Qiguo();
+        abstract public void ShaoYou();
+        abstract public void FangtShui();
+
+        public HongShaoRou Get() {
+            hongshaorou = new HongShaoRou();
+            Qiguo(); // 起锅
+            ShaoYou(); // 烧油
+            FangtShui(); // 放葱姜蒜
+            return hongshaorou;
+        }
+    }
+
+    public class ChuanCai : HongshaorouBuilder { // 川菜
+        public override void FangtShui()
+        {
+            hongshaorou.Shui = 100;
+        }
+
+        public override void Qiguo()
+        {
+            hongshaorou.Wendu = 800;
+        }
+
+        public override void ShaoYou()
+        {
+            hongshaorou.You = 500;
+        }
+    }
+    
+    public class Client {
+        public void ZuoHongshaorou()
+        {
+            var cuanCai = new ChuanCai();
+            HongShaoRou cuancaiHongshaorou = cuanCai.Get();
+            Console.WriteLine("wendu:{0}, shui:{1}, you: {2}", cuancaiHongshaorou.Wendu, cuancaiHongshaorou.Shui, cuancaiHongshaorou.You);
+        }
+    }
+}
--- DesignMode/Program.cs
+++ DesignMode/Program.cs
@@ -6,10 +6,7 @@ namespace DesignMode
     {
         static void Main(string[] args)
         {
-            Application app1 = new Application1();
-            Application app2 = new Application2();
-            Client client = new Client(app2);
-            client.Excute();
+            new DesignMode_Builder.Client().ZuoHongshaorou();
             Console.ReadKey();
         }
     }
