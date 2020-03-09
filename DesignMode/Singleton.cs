e1260ab7b64da2d294b6c3b463d7bbe09e5c1ef2
Just-Doing <969502899@qq.com>
2020-03-09 23:30:05 +0800

    单例 多种方式实现

--- DesignMode/Singleton.cs
+++ DesignMode/Singleton.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace DesignMode_Singleton
 {
+    // 懒汉模式 线程安全 效率低
     public class Singleton
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        protected Singleton() { }
+        private Singleton() { }
         private static Singleton _instance;
         public static Singleton GetInstance() {
             if (_instance == null) {
@@ -18,9 +19,24 @@ namespace DesignMode_Singleton
         }
     }
 
-    public class Client {
-        public void Test() {
-            var signleton = Singleton.GetInstance() ;
+    // 饿汉模式 线程安全 效率高
+    public class Singleton1
+    {
+        //    使用final修饰是防止被继承
+        private static Singleton1 singleton = new Singleton1();
+        private Singleton1() { }
+        public static Singleton1 getInstance()
+        {
+            return singleton;
+        }
+    }
+
+
+    public class Client
+    {
+        public void Test()
+        {
+            var signleton = Singleton.GetInstance();
 
             signleton.Name = "张三";
             signleton.Age = 11;
