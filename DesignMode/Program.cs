e4218e3f869e4085965465a4af202dbbf7975628
Just-Doing <969502899@qq.com>
2020-03-09 23:07:56 +0800

    原型拷贝

--- DesignMode/Program.cs
+++ DesignMode/Program.cs
@@ -6,7 +6,7 @@ namespace DesignMode
     {
         static void Main(string[] args)
         {
-            new DesignMode_Builder.Client().ZuoHongshaorou();
+            new DesignMode_Prototype.Client().Test();
             Console.ReadKey();
         }
     }
---
+++ DesignMode/Prototype.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignMode_Prototype
+{
+    public class Prototype: ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone() ; // 浅复制
+        }
+    }
+    public class Client
+    {
+        public void Test() {
+            var t1 = new Prototype();
+            t1.Name = "张三";
+            t1.Age = 12;
+
+            var t2 = (Prototype)t1.Clone();
+
+            Console.WriteLine(t1.Name);
+            Console.WriteLine(t2.Name);
+            Console.WriteLine(t2.Age);
+        }
+    }
+
+}
