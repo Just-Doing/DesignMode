dad5811ac260ad2ffed14662042df95cb100fc6b
yanglin <yanglin@sharewinfobi.onmicrosoft.com>
2020-03-12 21:40:51 +0800

    适配器模式

--- DesignMode/Adapter.cs
+++ DesignMode/Adapter.cs
@@ -4,27 +4,62 @@ using System.Text;
 
 namespace DesignMode
 {
-    public class Adapter
+    #region 单继承
+    public class TargetA
     {
-        public void Excute()
+        public virtual void Excute()
         {
             Console.WriteLine("这是 目标 事件");
         }
     }
 
-    // 适配器
-    public class Target
+    public class TargetA_Adapter : TargetA
     {
-        Adapter adapter;
-        public Target(Adapter apapter) {
-            this.adapter = apapter;
-        }
-
-        public void NewFun() {
+        public override void Excute()
+        {
             Console.WriteLine("干点事情。。---在目标方法前");
-            this.adapter.Excute();
+            base.Excute();
             Console.WriteLine("干点事情。。---在目标方法后");
         }
 
     }
+
+    #endregion
+
+
+    #region 多接口集成
+
+    public class TargetB 
+    {
+        public virtual void Excute()
+        {
+            Console.WriteLine("这是 目标 事件");
+        }
+    }
+
+    public abstract class Adapter: TargetB
+    {
+        public abstract void BeforeDoSomething();
+        public abstract void EndDoSomething();
+        public override void Excute()
+        {
+            BeforeDoSomething();
+            base.Excute();
+            EndDoSomething();
+        }
+    }
+
+    public class TargetB_Adapter : Adapter
+    {
+        public override void BeforeDoSomething()
+        {
+            Console.WriteLine("干点事情。。---在目标方法前");
+        }
+
+        public override void EndDoSomething()
+        {
+            Console.WriteLine("干点事情。。---在目标方法后");
+        }
+    }
+    #endregion
 }
