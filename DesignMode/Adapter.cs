987d537c426421b7b8eab97505a81acd4103160e
yanglin <yanglin@sharewinfobi.onmicrosoft.com>
2020-03-12 21:09:46 +0800

    适配器

---
+++ DesignMode/Adapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignMode
+{
+    public class Target
+    {
+        public void Excute()
+        {
+            Console.WriteLine("这是 目标 事件");
+        }
+    }
+
+    // 适配器
+    public class Adapter {
+        Target target;
+        public Adapter(Target t) {
+            this.target = t;
+        }
+
+        public void NewFun() {
+            Console.WriteLine("干点事情。。---在目标方法前");
+            this.target.Excute();
+            Console.WriteLine("干点事情。。---在目标方法后");
+        }
+
+    }
+}
